2e2536230f16c193af165454093e5fdb3e280b36
Mathieu GRUSON <mgruson@e1r7p16.42.fr>
2022-05-09 17:42:19 +0200

    memmove

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: mgruson <mgruson@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 14:50:53 by mgruson           #+#    #+#             */
-/*   Updated: 2022/05/09 16:47:49 by mgruson          ###   ########.fr       */
+/*   Updated: 2022/05/09 17:41:32 by mgruson          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ void *ft_memmove(void *dest, const void *src, size_t n);
 */
 int main () 
 {
+	merci bb
     char    *ma;
     char    *tab1[10] = "abcdefghyg";
 
