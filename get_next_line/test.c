411590755b56044b81f877027d0a048ab621c750
Mathieu GRUSON <mgruson@e3r3p3.42.fr>
2022-06-28 12:56:26 +0200

    test

--- get_next_line/test.c
+++ get_next_line/test.c
@@ -1,6 +1,28 @@
+#include <unistd.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
-int	main(int argc, char *argv[])
+int	main()
 {
+	int	fd;
+	char buf[5000];
+	
+	fd = 0;
+	fd = open("text.txt", O_RDONLY);
+
+	if (fd == -1)
+	{
+		printf("test failed");
+		return (1);
+	}
+	
+	read(fd, buf, 2000);
+	buf[5000] = '\0';
+	close(fd);
+
+	while()
+	printf("%c", buf[10]);
 	return 0;
 }
