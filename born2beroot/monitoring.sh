91ce30b38d70ec00a5b09fd24b1820682af211ef
Mathieu GRUSON <mgruson@e1r8p14.clusters.42paris.fr>
2022-07-18 19:35:33 +0200

    born2beroot finish

--- born2beroot/monitoring.sh
+++ born2beroot/monitoring.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
-wall "#Architecture: $(uname -a)
-#CPU physical: $(grep "physical id" /proc/cpuinfo | uniq | wc -l)
-#vCPU: $(grep "processor" /proc/cpuinfo | wc -l)
-#Memory Usage: $(free -m | awk 'NR==2{printf "%s/%sMB (%.2f%%)", $3,$2,$3*100/$2 }')
-#Disk Usage: $(df -h | awk '$NF=="/"{printf "%d/%dGB (%s)", $3,$2,$5}')
-#CPU load: $(top -bn1 | grep '%Cpu' | cut -c 9- | xargs | awk '{printf("%.1f%%"), $1 + $3}')
-#Last boot: $(who -b | awk '{print $3" "$4}')
-#LVM use: $(lsblk |grep lvm | awk '{if ($1) {print "yes";exit;} else {print "no"} }')
-#Connexions TCP: $(netstat -an | grep ESTABLISHED |  wc -l) ESTABLISHED
-#User log: $(users | wc -w)
-#Network: IP $(hostname -I) ($(ip a | grep link/ether | awk '{print $2}'))
-#Sudo: $(journalctl _COMM=sudo | grep COMMAND | wc -l) cmd"
+#sleep $(bc <<< $(uptime -s | cut -d ":" -f2)%10*60+$(uptime -s | cut -d ":" -f3)) && 
+wall -n " 
+                                         * *
+                                          *
+Date : $(date)
+
+- Architecture                     : $(uname -a)
+- Number of physical CPUs          : $(grep 'physical id' /proc/cpuinfo | uniq | wc -l)
+- Number of virtual CPUs           : $(grep processor /proc/cpuinfo | uniq | wc -l)
+- RAM available                    : $(free --mega | grep Mem | awk '{print $7" Mo"}')
+- Percentage of RAM uses           : $(free | grep Mem | awk '{print($3/$2*100)"%"}')
+- Memory available                 : $(df -h --total | grep total | awk '{print $4}')
+- Percentage of memory used        : $(df -h --total | grep total | awk '{print $5}')
+- Percentage of processors used    : $(top -bn1 | grep '^%Cpu(s)' | awk '{print(100 - $8)"%"}')
+- Last reboot                      : $(who -b | grep system | awk '{print $3 " " $4}')
+- LVM activity                     : $(if [ $(lsblk |grep lvm | wc -l) > 1 ]; then echo on; else echo off; fi)
+- Number of active connections     : $(grep TCP /proc/net/sockstat | awk '{print $3 " established"}')
+- Number of users using the server : $(echo $(who | wc -l) "user")
+- The IPv4 adress of server        : $(ip a | grep 127 | awk '{print $2}')
+- MAC adress of the server         : $(ip link show | grep link/ether | awk '{print $2}')
+- Number of sudo commands executed : $(grep --text 'COMMAND' /var/log/sudo/sudo.log | wc -l)
+
+                                        * *
